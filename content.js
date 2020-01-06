3d9bbcfb855650bce05978193c69f22930d8b853
notexplosive <thenotexplosive@gmail.com>
2020-01-06 00:18:55 -0800

    Related videos

--- content.js
+++ content.js
@@ -1,9 +1,7 @@
-function filterOnSubscriptionsOrChannel() {
-  var videos = getVideoElements();
-
+function filterVideoElements() {
   for (video of videos) {
-    var oldTitle = video.titleElement.getAttribute("title");
-    video.titleElement.innerHTML = createNewTitle(oldTitle);
+    var oldTitle = video.titleText;
+    video.titleElement.innerText = createNewTitle(oldTitle);
 
     if (oldTitle != video.titleElement.innerHTML) {
       video.titleElement.style.color = "#E5862D";
@@ -24,7 +22,9 @@ function createNewTitle(oldTitle) {
     endPartOfTitle == "Game Grumps Compilations" ||
     endPartOfTitle == "Game Grumps" ||
     endPartOfTitle == "Guest Grumps" ||
-    endPartOfTitle == "Ten Minute Power Hour"
+    endPartOfTitle == "Ten Minute Power Hour" ||
+    endPartOfTitle == "Steam Train" ||
+    endPartOfTitle == "Game Grumps VS"
   ) {
     return oldTitle;
   }
@@ -32,7 +32,7 @@ function createNewTitle(oldTitle) {
   return endPartOfTitle;
 }
 
-function getVideoElements() {
+function getVideoElementsForChannelOrSubs() {
   videos = [];
 
   var videoElements = document.getElementsByClassName(
@@ -45,18 +45,55 @@ function getVideoElements() {
       linkElements = element.querySelectorAll("a");
       if (linkElements.length > 0) {
         var possibleAuthorElement = getAuthorElement(linkElements);
+        var titleElement = getTitleElement(linkElements);
         // if authorElement is null then we might be looking at the channel page or a video
         if (
           possibleAuthorElement == null ||
           possibleAuthorElement.innerHTML === "GameGrumps"
         ) {
           var video = {
-            titleElement: getTitleElement(linkElements),
-            authorElement: possibleAuthorElement
+            titleElement: titleElement,
+            authorElement: possibleAuthorElement,
+            titleText: titleElement.getAttribute("title")
           };
           videos.push(video);
         }
       }
+    }
+  }
+
+  return videos;
+}
+
+function getVideoElementsForWatch() {
+  videos = [];
+
+  var secondaryVideoElements = document.getElementsByClassName(
+    "style-scope ytd-watch-next-secondary-results-renderer"
+  );
+
+  var i = 0;
+  for (element of secondaryVideoElements) {
+    i++;
+    if (i < 2) {
+      continue;
+    }
+
+    var titleElementRoot = element.querySelectorAll("h3")[0];
+
+    if (titleElementRoot) {
+      var titleElement = titleElementRoot.querySelectorAll("span")[0];
+      var video = {
+        titleElement: titleElement,
+        authorElement: element.querySelectorAll("ytd-channel-name")[0],
+        titleText: titleElement.innerText
+      };
+
+      if (video.authorElement.innerText === "GameGrumps") {
+        videos.push(video);
+      }
+    } else {
+      // playlist probably
     }
   }
 
@@ -86,8 +123,18 @@ function getAuthorElement(linksElements) {
   return null;
 }
 
+var exec = null;
+
 if (document.URL.match(/https:\/\/www\.youtube\.com\/watch.*/gm)) {
-  filterRelatedVideos();
+  exec = function() {
+    var videos = getVideoElementsForWatch();
+    filterVideoElements(videos);
+  };
 } else {
-  filterOnSubscriptionsOrChannel();
+  exec = function() {
+    var videos = getVideoElementsForChannelOrSubs();
+    filterVideoElements(videos);
+  };
 }
+
+setInterval(exec, 500);
